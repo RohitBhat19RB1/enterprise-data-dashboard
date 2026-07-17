24b804c1e888aa0bf196b7c61f7171a28170b731
RohitBhat19RB1 <rohitbhat16860@gmail.com>
2026-07-18 00:54:32 +0530

    chore: install ag-grid-react and ag-grid-community core modules

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
-import { useState } from 'react'
-import './App.css'
+import React from "react";
+import './App.css';
+import MainDashboard from "./components/MainDashboard";
 
 function App() {
 
   return (
-    <>
-    </>
-  )
+    <div>
+      <MainDashboard />
+    </div>
+  );
 }
 
 export default App
---
+++ src/components/MainDashboard.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function MainDashboard() {
+  return (
+    <div className="p-8">
+      <h1 className="text-xl font-bold text-slate-800">Dashboard Active</h1>
+    </div>
+  );
+}
